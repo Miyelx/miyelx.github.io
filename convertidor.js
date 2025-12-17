00481d693312b845ab81698c91c37c7b6add43cf
Miyelx <migue.26a20@gmail.com>
2025-12-17 10:12:46 -0400

    Change COP to USD exchange rate
    Updated the exchange rate for COP to USD.

--- convertidor.js
+++ convertidor.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   //precios nuevos
     const bsToUsd = 276.5769;//precios de las divisas (dolar)
     const bsToEur = 326.15607509;//euros
-    const copToUsd = 3817.59;// dolar (peso colombiano)     
+    const copToUsd = 3847.10;// dolar (peso colombiano)     
   
   const euroCop = (bsToEur / bsToUsd) * copToUsd;//calcular euro en base al precio del dolar para pesos 
   // Venezuela
