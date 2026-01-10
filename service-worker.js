f55bbb0bcd22831e90229da693543d88109ef786
Miyelx <migue.26a20@gmail.com>
2026-01-09 21:00:33 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cache-v7.22"; // cambia versión al actualizar
+const CACHE_NAME = "cache-v7.23"; // cambia versión al actualizar
 
 // Instalación: cachear recursos iniciales
 self.addEventListener("install", event => {
