1fb0627d1496bdef08acdc36210b8a53191df72b
Miyelx <migue.26a20@gmail.com>
2026-01-28 14:15:34 -0400

    Refactor options assignment in sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10291979
-}
+self.options = { "domain": "5gvci.com", "zoneId": 10291979 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
