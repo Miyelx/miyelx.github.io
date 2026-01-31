423db99caa28618b966e9abc88c2a1445617b43d
Miyelx <migue.26a20@gmail.com>
2026-01-31 17:57:05 -0400

    Update cache version and limit cache size

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cache-v7.5";
+const CACHE_NAME = "cache-v7.6";
 const ASSETS = [
   "./", 
   "sw.js",
@@ -62,7 +62,7 @@ self.addEventListener("fetch", event => {
         return response || fetch(request).then(netResponse => {
           return caches.open(CACHE_NAME).then(cache => {
             cache.put(request, netResponse.clone());
-            limitarCache(CACHE_NAME, 50);
+            limitarCache(CACHE_NAME, 48);
             return netResponse;
           });
         });
